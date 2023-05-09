83b860b118fb71434c4a9e63ff0e62f75b20de30
GreatEki <ekeneidiagbor@gmail.com>
2023-05-09 15:37:55 +0100

    feat: User created listener at order ms

---
+++ order/events/listerners/UserCreatedListener.ts
@@ -0,0 +1,29 @@
+import {
+  BaseListener,
+  RoutingKey,
+  UserCreatedEvent,
+  UserType,
+} from "@greateki-ticket-ms-demo/ecommshared";
+import { serviceQueueName } from "../queue-name";
+import { ConsumeMessage } from "amqplib";
+import prisma from "../../config/prisma-client";
+
+export default class UserCreatedListener extends BaseListener<UserCreatedEvent> {
+  routingKey: RoutingKey.UserCreated = RoutingKey.UserCreated;
+  QueueName: string = serviceQueueName;
+
+  async onMessage(
+    data: UserCreatedEvent["message"],
+    msg: ConsumeMessage
+  ): Promise<void> {
+    // confirm user is customer
+    if (!data.userType?.includes(UserType.CUSTOMER)) {
+      this.channel.ack(msg);
+      return;
+    }
+
+    await prisma.user.create({ data: { ...data } });
+
+    this.channel.ack(msg);
+  }
+}
---
+++ order/events/queue-name.ts
@@ -0,0 +1,1 @@
+export const serviceQueueName = "order-service";
