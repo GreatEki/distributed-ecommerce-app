2627b7edfd1370140ce53b4536c4d1996fe4c77e
GreatEki <ekeneidiagbor@gmail.com>
2023-05-09 12:37:43 +0100

    feat: completed add to cart & get user cart

--- product/lib/cart/cart.controller.ts
+++ product/lib/cart/cart.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import prisma from "../../config/prisma-client";
 import {
+  BadRequestError,
   ForbiddenError,
   NotFoundError,
   UserType,
@@ -23,21 +24,51 @@ export const addItemToCart = async (
     // get users cart
     const userCart = await prisma.cart.findUnique({
       where: { userId: req.currentUser!.id },
-      include: { user: true },
+      include: { user: true, products: true },
     });
 
     if (!userCart || !userCart.user.userType?.includes(UserType.CUSTOMER))
       throw new ForbiddenError("Invalid user");
 
+    const CartItems = userCart.products;
+
+    const existingItem = CartItems.find((item) => item.id === productId);
+
+    if (existingItem) throw new BadRequestError("Item is already in cart");
+
     //   add to cart
     const result = await prisma.cart.update({
       where: { userId: userCart.userId },
-      data: { products: { set: [{ id: productId }] } },
+      data: { products: { connect: [{ id: productId }] } },
     });
 
     return res.json({
       message: "Item added to cart",
       data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getUserCart = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { userId } = req.params;
+
+    const cart = await prisma.cart.findUnique({
+      where: { userId },
+      include: { products: true },
+    });
+
+    if (!cart) throw new NotFoundError("No cart found for record user");
+
+    return res.json({
+      message: "User cart info",
+      data: cart,
     });
   } catch (err) {
     next(err);
--- product/lib/cart/cart.router.ts
+++ product/lib/cart/cart.router.ts
@@ -4,5 +4,6 @@ import * as controller from "./cart.controller";
 const CartRouter = Router();
 
 CartRouter.route("/add").post(controller.addItemToCart);
+CartRouter.route("/user/:userId").get(controller.getUserCart);
 
 export default CartRouter;
